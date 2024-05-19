cf22ffeee419369b006d31826cac1d3be994b7fb
chase104 <62479547+chase104@users.noreply.github.com>
2024-05-19 11:25:39 -0500

    Setup basic frontend routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,33 @@
 import { useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Auth from "./pages/Auth";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   // get user from redux toolkit slice
+  let user = false;
 
   useEffect(() => {
-    axios("https://4437y2tox5.execute-api.us-east-1.amazonaws.com/").then(
-      console.log
-    );
-    axios("https://4437y2tox5.execute-api.us-east-1.amazonaws.com/two").then(
-      console.log
-    );
+    axios("https://4437y2tox5.execute-api.us-east-1.amazonaws.com/")
+      .then(console.log)
+      .catch(console.error);
+    axios("https://4437y2tox5.execute-api.us-east-1.amazonaws.com/two")
+      .then(console.log)
+      .catch(console.error);
   }, []);
-
+  if (user) return <Auth />;
   return (
     // basic react router dom routing for pages
-    <div></div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/product/:productId" element={<ProductPage />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
   );
 }
 
